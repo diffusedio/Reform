31b4187a3034543ec6fcb0b2c37a3d2a95858104
Daniel Grimes <dan@dcgnet.co.uk>
2021-03-15 13:29:16 +0000

    brin octopus hcl parser into project

---
+++ src/Octopus.Core.Parsers.Hcl/HclListPropertyElement.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Octopus.CoreParsers.Hcl
+{
+    /// <summary>
+    /// Represents a list assigned to a property
+    /// </summary>
+    public class HclListPropertyElement : HclListElement
+    {
+        public override string Type => ListPropertyType;
+
+        public override string Value => PrintArray(-1);
+
+        public override string ToString(bool naked, int indent)
+        {
+            if (naked)
+            {
+                return base.ToString(true, indent);
+            }
+
+            var indentString = GetIndent(indent);
+            return indentString + OriginalName + " = " + PrintArray(indent);
+        }
+    }
+}
--- src/Reform.Core/Class1.cs
+++ src/Reform.Core/Class1.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Reform.Core
 {
     public class Class1
     {
+
+        public void Parse(string src)
+        {
+
+            Hcl
+
+        }
+
+
+
     }
 }
