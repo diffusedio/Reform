831347478682c95c4910d82728f489fb20f0429e
Daniel Grimes <dan@dcgnet.co.uk>
2021-03-15 12:15:50 +0000

    useful test functions

--- tests/Reform.Test/UnitTest1.cs
+++ tests/Reform.Test/UnitTest1.cs
@@ -1,13 +1,30 @@
-using System;
+using Reform.Test.Utils;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Reform.Test
 {
     public class UnitTest1
     {
+
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
         [Fact]
         public void Test1()
         {
+
+
+
+            var result = new object();
+
+
+            output.Dump(result);
 
         }
     }
